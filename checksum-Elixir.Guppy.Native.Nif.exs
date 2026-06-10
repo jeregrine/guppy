3a2f9f3e6def694073bc2e941cee549dc92f1799
Jason Stiebs <stiebs.81@gmail.com>
2026-06-10 12:16:21 -0500

    Update checksums for 0.1.1 and skip download smoke before artifacts publish

--- checksum-Elixir.Guppy.Native.Nif.exs
+++ checksum-Elixir.Guppy.Native.Nif.exs
@@ -1,3 +1,3 @@
 %{
-  "libguppy_nif-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:2b06684e94e740ddb60592a21167c6d0db862cdf318239da0284336f63eb9d0e",
+  "libguppy_nif-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:388677dcd2fb924c555eb9eba56cb7b5fa47a526705992b176e9cc317cfb10c4",
 }
